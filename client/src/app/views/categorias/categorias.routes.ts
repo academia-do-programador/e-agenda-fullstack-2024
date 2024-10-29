8cc1225bfd74660acbe0d7d9bd0f962ee6efc1b6
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-10-29 11:05:33 -0300

    Implementada edição de categorias

--- client/src/app/views/categorias/categorias.routes.ts
+++ client/src/app/views/categorias/categorias.routes.ts
@@ -1,27 +1,46 @@
-import { ResolveFn, Routes } from "@angular/router";
-import { ListarCategoriaViewModel } from "./models/categoria.models";
-import { inject } from "@angular/core";
-import { CategoriaService } from "./services/categoria.service";
-import { ListagemCategoriasComponent } from "./listar/listagem-categorias.component";
-import { CadastroCategoriaComponent } from "./cadastrar/cadastro-categoria.component";
+import { ActivatedRouteSnapshot, ResolveFn, Routes } from '@angular/router';
+import {
+  ListarCategoriaViewModel,
+  VisualizarCategoriaViewModel,
+} from './models/categoria.models';
+import { inject } from '@angular/core';
+import { CategoriaService } from './services/categoria.service';
+import { ListagemCategoriasComponent } from './listar/listagem-categorias.component';
+import { CadastroCategoriaComponent } from './cadastrar/cadastro-categoria.component';
+import { EdicaoCategoriaComponent } from './editar/edicao-categoria.component';
 
-const listagemCategoriasResolver: ResolveFn<ListarCategoriaViewModel[]> = () => {
+const listagemCategoriasResolver: ResolveFn<
+  ListarCategoriaViewModel[]
+> = () => {
   return inject(CategoriaService).selecionarTodos();
+};
+
+const visualizarCategoriaResolver: ResolveFn<VisualizarCategoriaViewModel> = (
+  route: ActivatedRouteSnapshot
+) => {
+  const id = route.params['id'];
+
+  return inject(CategoriaService).selecionarPorId(id);
 };
 
 export const categoriasRoutes: Routes = [
   {
-    path : '',
+    path: '',
     redirectTo: 'listar',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'listar',
     component: ListagemCategoriasComponent,
-    resolve: { categorias: listagemCategoriasResolver }
+    resolve: { categorias: listagemCategoriasResolver },
   },
   {
     path: 'cadastrar',
     component: CadastroCategoriaComponent,
-  }
-]
+  },
+  {
+    path: 'editar/:id',
+    component: EdicaoCategoriaComponent,
+    resolve: { categoria: visualizarCategoriaResolver },
+  },
+];
