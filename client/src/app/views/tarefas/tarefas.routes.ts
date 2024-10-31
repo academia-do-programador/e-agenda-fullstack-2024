f847d7f51fa18c01447603ae68cddb21acf5f63c
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-10-31 11:54:41 -0300

    Implementado cadastro de tarefa

--- client/src/app/views/tarefas/tarefas.routes.ts
+++ client/src/app/views/tarefas/tarefas.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ListagemTarefasComponent } from './listar/listagem-tarefas.component';
 import { listagemTarefasResolver } from './services/listagem-tarefas.resolver';
+import { CadastroTarefaComponent } from './cadastrar/cadastro-tarefa.component';
 
 export const tarefasRoutes: Routes = [
   { path: '', redirectTo: 'listar', pathMatch: 'full' },
@@ -9,10 +10,10 @@ export const tarefasRoutes: Routes = [
     component: ListagemTarefasComponent,
     resolve: { tarefas: listagemTarefasResolver },
   },
-  // {
-  //   path: 'cadastrar',
-  //   component: CadastroTarefaComponent,
-  // },
+  {
+    path: 'cadastrar',
+    component: CadastroTarefaComponent,
+  },
   // {
   //   path: 'editar/:id',
   //   component: EdicaoTarefaComponent,
