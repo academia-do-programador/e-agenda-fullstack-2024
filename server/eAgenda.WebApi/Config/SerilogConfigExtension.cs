6aa0acb39ab0ddcdd3e1654939d33d9ccbd66439
alexandre.rech.lages <alexandre.rech.lages@gmail.com>
2024-11-04 16:42:31 -0300

    Alterado o serilog para escrever no account blob da azure

--- server/eAgenda.WebApi/Config/SerilogConfigExtension.cs
+++ server/eAgenda.WebApi/Config/SerilogConfigExtension.cs
@@ -4,12 +4,17 @@ namespace eAgenda.WebApi.Config
 {
     public static class SerilogConfigExtension
     {
-        public static void ConfigurarSerilog(this IServiceCollection services, ILoggingBuilder logging)
+        public static void ConfigurarSerilog(this IServiceCollection services, ILoggingBuilder logging, IConfiguration configuration)
         {
+            string connectionStringLogs = configuration.GetConnectionString("AccountLogs");
+
             Log.Logger = new LoggerConfiguration()
               .MinimumLevel.Information()
               .Enrich.FromLogContext()
-              .WriteTo.Console()
+              .WriteTo.AzureBlobStorage(
+                connectionString: connectionStringLogs,
+                storageContainerName: "logs",
+                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
               .CreateLogger();
 
             Log.Logger.Information("Iniciando aplicação...");
