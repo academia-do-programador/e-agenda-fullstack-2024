ad46bfa895438735d7f291db69272aa0187e490d
alexandre.rech.lages <alexandre.rech.lages@gmail.com>
2024-11-05 14:50:08 -0300

    Configurado "new relic"

--- server/eAgenda.WebApi/Config/SerilogConfigExtension.cs
+++ server/eAgenda.WebApi/Config/SerilogConfigExtension.cs
@@ -6,31 +6,18 @@ namespace eAgenda.WebApi.Config
     {
         public static void ConfigurarSerilog(this IServiceCollection services, ILoggingBuilder logging)
         {
-            var connectionStringLogs = Environment.GetEnvironmentVariable("AccountLogs");
-
-            if (string.IsNullOrEmpty(connectionStringLogs) == false)
-            {
-                Log.Logger = new LoggerConfiguration()
-                 .MinimumLevel.Information()
-                 .Enrich.FromLogContext()
-                 .WriteTo.AzureBlobStorage(
-                   connectionString: connectionStringLogs,
-                   storageContainerName: "logs",
-                   outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
-                 .CreateLogger();               
-            }        
-            else
-            {
-                Log.Logger = new LoggerConfiguration()
-                  .MinimumLevel.Information()
-                  .Enrich.FromLogContext()
-                  .WriteTo.Console()
-                  .CreateLogger();
-            }
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+                .Enrich.WithThreadId()    
+                .WriteTo.Console()
+                .WriteTo.NewRelicLogs(
+                    endpointUrl: "https://log-api.newrelic.com/log/v1",
+                    applicationName: "Serilog.Sinks.NewRelic.eAgenda",
+                    licenseKey: "d1cf0d4e74171c854c3089ae04067b61FFFFNRAL")
+                .CreateLogger();
 
             Log.Logger.Information("Iniciando aplicação...");
-
-            Log.Logger.Information("Chave de logs: " + connectionStringLogs);
 
             logging.ClearProviders();
 
